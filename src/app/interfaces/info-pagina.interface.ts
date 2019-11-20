e05c7a9aeebafe3d1f86f40410244a4b084e070c
Jesus Gibran <rayajggcc@hotmail.com>
2019-11-20 16:43:27 -0600

    Fin de la seccion 6

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,15 @@
+
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  Nombre_corto?: string;
+  pagina_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  equipo_trabajo?: any[];
+}
+
+
+
+
---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+
+  equipo: any[] = [];
+
+  constructor( private http: HttpClient) {
+
+    this.cargarInfo();
+    this.cargarEquipo();
+    // console.log('Servicio de info Listo');
+    // leer el archivo json
+    // console.log( resp['titulo']);
+
+
+  }
+  private cargarInfo() {
+    // Leer archivos JSON
+    this.http.get('assets/data/data_pagina.json')
+    .subscribe ( (resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+
+
+    });
+
+
+  }
+
+  private cargarEquipo() {
+
+    this.http.get('https://angular-html-5b92d.firebaseio.com/equipo.json')
+    .subscribe ( (resp: any[]) => {
+
+      this.equipo = resp;
+      console.log(resp);
+
+
+  });
+}
+}
